b74a5ae618a7d29980590d33725ceff3475c796d
kimsanghee <tkdgmltjddnr@naver.com>
2022-03-19 11:15:24 +0900

    혼자 한 es5

--- js/slider.js
+++ js/slider.js
@@ -1,66 +1,70 @@
-const slider = document.querySelector('#slider');
-const ul = slider.querySelector('ul');
-const lis = ul.querySelectorAll('li');
-const prev = slider.querySelector('.prev');
-const next = slider.querySelector('.next');
+const slider1 = document.querySelector('#slider');
+const slider2 = document.querySelector('#slider2');
+const prev = document.querySelector('.prev');
+const next = document.querySelector('.next');
 const speed = 500;
-const len = ul.children.length;
 let enableClick = true;
 
-init();
-//무한 루프 만들기
-next.addEventListener('click',function(e){
+init(slider1);
+init(slider2);
+
+next.addEventListener('click',e => {
     e.preventDefault();
+
     if(enableClick){
         enableClick = false;
-        //nextSlide();
-        slide('next');
+        slide(slider1,'next');
+        slide(slider2,'next');
     }
-    
 });
-prev.addEventListener('click',function(e){
+
+prev.addEventListener('click',e => {
     e.preventDefault();
+
     if(enableClick){
         enableClick = false;
-        //prevSlide();
-        slide('prev');
+        slide(slider1,'prev');
+        slide(slider2,'prev');
     }
 });
 
 
-function init(){
-    //반복 돌기위해 첫번째 화면을 뒤로 보내라
+function init(frame){
+    const ul = frame.querySelector('ul');
+    const lis = ul.querySelectorAll('li');
+    const len = ul.children.length;
     ul.style.left = '-100%';
-    //li가 추가될떄마다 ul의 넓이 값이 변경될 수 있도록 작업
     ul.style.width = `${100*len}%`;
-    lis.forEach(function(li){
-         //li가 추가될떄마다 li의 각각의 넓이 값이 변경될 수 있도록 작업
-        li.style.width = `${100/len}%`
-    });
+    lis.forEach (li =>li.style.width = `${100/len}%`);
     ul.prepend(ul.lastElementChild);
 }
 
-function slide(direction){
-    let result = {value : '', callback : null}
-    if(direction == 'next'){
+function slide(frame,direction){
+    const ul = frame.querySelector('ul');
+
+    let result =  {
+        value: '', callback: null
+    }
+    if(direction === 'next'){
         result.value = '-200%';
-        result.callback = ()=>{ul.append(ul.firstElementChild)};
+        result.callback = ()=>{ ul.append(ul.firstElementChild)}
     }
-    else if(direction == 'prev'){
-        result.value =' 0%';
-        result.callback = ()=>{ul.prepend(ul.lastElementChild)};
+    else if(direction === 'prev'){
+        result.value = '0%';
+        result.callback = ()=>{ ul.prepend(ul.lastElementChild)}
     }
-    else{
-        alert('인수는 prev,next 둘중하나 들어와야합니다.')
+    else {
+      alert ('인수는 prev, next둘중 하나가 들어와야 합니다.')
     }
-    new Anim(ul,{
-        prop : 'left',
-        value : result.value,
-        duration : speed,
-        callback : ()=>{
-            result.callback();
-            ul.style.left = '-100%';
-            enableClick = true;
-        }
-    });
+    new Anim(ul, {
+      prop: 'left',
+      value: result.value,
+      duration: speed,
+      callback: () => {      
+        result.callback();
+        ul.style.left = '-100%';
+        enableClick = true;
+      }
+    })
+  
 }
--- js/slider_es5.js
+++ js/slider_es5.js
@@ -1,39 +1,77 @@
 function Slider(){
-    this.init();
-    this.nextBtn();
-    this.prevBtn();
+    this.init1();
+    this.init2(this.slider1);
+    this.init2(this.slider2);
+    this.slide(frame,direction);
+    this.next();
+    this.prev();
 }
-Slider.prototype.init = function(){
-    this.slider = document.querySelector('#slider');
-    this.ul = this.slider.querySelector('ul');
-    this.prev = this.slider.querySelector('.prev');
-    this.next = this.slider.querySelector('.next');
+Slider.prototype.init1 = function(){
+    this.slider1 = document.querySelector('#slider');
+    this.slider2 = document.querySelector('#slider2');
+    this.prev = document.querySelector('.prev');
+    this.next = document.querySelector('.next');
     this.speed = 500;
-    this.ul.style.left = '-100%';
+    this.enableClick = true;
 }
-Slider.prototype.nextBtn = function(){
-    this.next.addEventListener('click',function(){
-        new Anim(this.ul,{
-            prop : 'left',
-            value : '-200%',
-            duration : this.speed,
-            callback : function(){
-                this.ul.append(this.ul.firstElementChild);
-                this.ul.style.left = '-100%';
-            }.bind(this)
-        });
-    }.bind(this));
+
+Slider.prototype.init2 = function(frame){
+  this.ul = frame.querySelector('ul');
+  this.lis = this.ul.querySelectorAll('li');
+  this.len = this.ul.children.length;
+  this.ul.style.left = '-100%';
+  this.ul.style.width = `${100*this.len}%`;
+  this.lis.forEach (li =>li.style.width = `${100/this.len}%`);
+  this.ul.prepend(this.ul.lastElementChild);
 }
-Slider.prototype.prevBtn = function(){
-    this.prev.addEventListener('click',function(){
-        new Anim(this.ul,{
-            prop : 'left',
-            value : '0%',
-            duration : this.speed,
-            callback : function(){
-                this.ul.prepend(this.ul.lastElementChild);
-                this.ul.style.left = '-100%';
-            }.bind(this)
-        });
-    }.bind(this));
+
+Slider.prototype.next = function(){
+  this.next.addEventListener('click',e => {
+    e.preventDefault();
+
+    if(enableClick){
+        enableClick = false;
+        slide(slider1,'next');
+        slide(slider2,'next');
+    }
+  });
+}
+Slider.prototype.prev = function(){
+  this.prev.addEventListener('click',e => {
+    e.preventDefault();
+
+    if(enableClick){
+        enableClick = false;
+        slide(slider1,'prev');
+        slide(slider2,'prev');
+    }
+  });
+}
+Slider.prototype.slide = function(frame,direction){
+    const ul = frame.querySelector('ul');
+    let result =  {
+        value: '', callback: null
+    }
+    if(direction === 'next'){
+        result.value = '-200%';
+        result.callback = ()=>{ ul.append(ul.firstElementChild)}
+    }
+    else if(direction === 'prev'){
+        result.value = '0%';
+        result.callback = ()=>{ ul.prepend(ul.lastElementChild)}
+    }
+    else {
+      alert ('인수는 prev, next둘중 하나가 들어와야 합니다.')
+    }
+    new Anim(ul, {
+      prop: 'left',
+      value: result.value,
+      duration: this.speed,
+      callback: () => {      
+        result.callback();
+        ul.style.left = '-100%';
+        enableClick = true;
+      }
+    })
+  
 }
